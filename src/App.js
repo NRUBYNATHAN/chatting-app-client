d52b9b020b1ed81932ebf9b61b29d1b21bed73ac
NRUBYNATHAN <rubynathan999@gmail.com>
2023-07-01 19:54:13 +0530

    render

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
-const socket = io.connect("http://localhost:3002");
+const socket = io.connect("https://rubychatapp.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
